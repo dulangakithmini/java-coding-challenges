01373e77b1796a435e92a58935378e32adf7ce55
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-03 04:50:16 +0530

    Change Area class to Circle class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Area calculator");
+        System.out.println("Area calculator.");
 
         Menu menu = new Menu();
+        menu.setMessage("Enter a choice");
 
-        switch (menu.showMenu()) {
-            case 1:
-                System.out.print("Enter the radius: ");
-                double radius = scanner.nextDouble();
-                Circle circle = new Circle(radius);
-                System.out.println("Area = " + circle.calculateArea(radius));
-                break;
+        List<String> choiceList = new ArrayList<String>();
+        choiceList.add("calculate area");
+        choiceList.add("quit");
+        menu.setChoiceList(choiceList);
 
-            default:
-                break;
+        while (true) {
+            switch (menu.showMenu()) {
+                case 1:
+                    System.out.print("Enter the radius: ");
+                    double radius = scanner.nextDouble();
+                    Circle circle = new Circle(radius);
+                    System.out.println("Area = " + circle.calculateArea(radius));
+                    System.out.println("");
+                    break;
+
+                default:
+                    break;
+            }
         }
-
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,13 +1,27 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
+    private String message;
+    private List<String> choiceList;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setChoiceList(List<String> choiceList) {
+        this.choiceList = choiceList;
+    }
 
     public int showMenu() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your choice: ");
-        System.out.println("Press 1 to calculate area");
-        System.out.println("Press 2 to exit");
+        System.out.println(this.message);
+
+        for (int i = 0; i < choiceList.size(); i++) {
+            System.out.println("Press " + (i + 1) + " to " + choiceList.get(i));
+        }
+
         int choice = scanner.nextInt();
 
         return choice;
